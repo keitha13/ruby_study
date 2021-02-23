7178831de009af3b2f3e2a5be0158f4758c7942f
keitha13 <keitha9452@gmail.com>
2021-02-23 14:26:12 +0900

    make the file for fizz_buzz

--- index.rb
+++ index.rb
@@ -1,10 +1,12 @@
-country = "us"
-if country == "japan"
-  puts "こんにちは"
-elsif country == "us"
-  puts "hello"
-elsif country == "italy"
-  puts "ciao"
-else
-  puts "???"
+def greeting(country)
+  return puts "countryを入力してください" if country.nil?
+  if country == "japan"
+    puts "こんにちは"
+  else
+    puts "hello"
+  end
 end
+
+greeting("japan")
+greeting("us")
+greeting(nil)
