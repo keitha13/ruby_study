4c72255f4f40a6375676a6ef6ad6cac91008b93c
keitha13 <keitha9452@gmail.com>
2021-02-23 22:35:33 +0900

    study string details

--- index.rb
+++ index.rb
@@ -1,12 +1,1 @@
-def greeting(country)
-  return puts "countryを入力してください" if country.nil?
-  if country == "japan"
-    puts "こんにちは"
-  else
-    puts "hello"
-  end
-end
-
-greeting("japan")
-greeting("us")
-greeting(nil)
+puts sprintf('%0.3f', 1.2)
