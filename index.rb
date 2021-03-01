6fb43503583ae0abaaf01c419529efdb7f40a3aa
keitha13 <keitha9452@gmail.com>
2021-03-01 16:56:38 +0900

    study range object

--- index.rb
+++ index.rb
@@ -1,8 +1,17 @@
-# numbers = [1,2,3,4,5]
-# new_numbers = []
-# numbers.each {|n| new_numbers << n*10}
-# p new_numbers
+def charge(age)
+  case age
+  when 0..5
+    0
+  when 6..12
+    300
+  when 13..18
+    600
+  else
+    1000
+  end
+end
 
-numbers = [1,2,3,4,5]
-new_numbers = numbers.map{|n| n*10}
-p new_numbers
+p charge(3)
+p charge(12)
+p charge(16)
+p charge(25)
