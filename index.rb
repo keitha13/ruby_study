62f5b45799f820e9db266a391e5788aa6964f888
keitha13 <keitha9452@gmail.com>
2021-03-01 10:53:16 +0900

    study array

--- index.rb
+++ index.rb
@@ -1,9 +1,5 @@
-def foo(time = Time.now, message = bar)
-  "time: #{time}, message: #{message}"
-end
+# quo_rem = 14.divmod(3)
+# puts "商=#{quo_rem[0]}, 余り=#{quo_rem[1]}"
 
-def bar
-  'BAR'
-end
-
-puts foo
+quotient, remainder = 14.divmod(3)
+puts "商=#{quotient}, 余り=#{remainder}"
