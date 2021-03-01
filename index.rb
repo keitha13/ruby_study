d527e438a21ddb0c5066d3329f5b7430b111d3be
keitha13 <keitha9452@gmail.com>
2021-03-01 11:38:47 +0900

    study block

--- index.rb
+++ index.rb
@@ -1,12 +1,13 @@
-# numbers = [1,2,3,4]
-# sum = 0
-# numbers.each do |n|
-#   sum += n
-# end
-# puts sum
-
-a = [1,2,3,1,2,3]
-a.delete_if do |n|
-  n.odd?
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even?? n*10:n
+  sum += sum_value
 end
-p a
+puts sum
+
+# a = [1,2,3,1,2,3]
+# a.delete_if do |n|
+#   n.odd?
+# end
+# p a
